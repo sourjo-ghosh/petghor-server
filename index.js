22d28f158d3b6b965761b688d1d7e8eefdcc15bb
sourjo-ghosh <joyghosh211@outlook.com>
2026-05-22 02:04:27 +0600

    add-pet form dashboard api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 8000;
-const { MongoClient, ServerApiVersion, Db } = require("mongodb");
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const dns = require("node:dns");
 const { connect } = require("node:http2");
@@ -27,9 +27,25 @@ async function run() {
     const db = client.db("petGhor");
     const petCollections = db.collection("allPets");
     app.get("/all-pets", async (req, res) => {
-      const cursor = petCollections.find();
+      const species = req.query.species;
+      const search = req.query.search;
+      let query = {};
+      if (species) {
+        query.species = {
+          $regex: species,
+          $options: "i",
+        };
+      }
+      if (search) {
+        query.petName = {
+          $regex: search,
+          $options: "i",
+        };
+      }
+
+      const cursor = petCollections.find(query);
       const allPets = await cursor.toArray();
-      console.log(allPets)
+      // console.log(allPets);
       res.json({
         success: true,
         message: "All pets fetched successfully",
@@ -38,8 +54,33 @@ async function run() {
       });
     });
 
+    app.get("/all-pets/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id)
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const SinglePet = await petCollections.findOne(query);
+      res.json({
+        success: true,
+        message: "Pet fetched successfully",
+        data: SinglePet,
+      });
+    });
+
+    app.post("/add-pet", async (req, res) => {
+      // const db = client.db("petghor");
+      const result = await petCollections.insertOne(req.body);
+      // res.send(result);
+      res.json({
+        success: true,
+        message: "Pet added successfully",
+        data: result,
+      });
+    });
+
     app.listen(port, () => {
-      console.log(`Server is running of ${port} ${uri}`);
+      console.log(`Server is running of ${port}`);
     });
   } finally {
     // Ensures that the client will close when you finish/error
